09f94ee16d8bcd214ba26562cf98d5427fde6484
w2moon <w2moon@gmail.com>
2016-04-14 17:31:07 +0800

    add express

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = function() {
+    // 输入你的配置
+    return {
+        appId: 'wx00803668d56e9937',
+        appSecret: '3062e63c8393880cd069aedfd735b3e6',
+        appToken: 'lovigame'
+    };
+};
